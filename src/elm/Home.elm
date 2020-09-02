00d5cd3ff8c443adcc7d72c54dd0dcbbc36afbe9
Kyosuke Fujimoto <kyoro.f@gmail.com>
2020-09-02 22:41:38 +0900

    Extract connections

--- src/elm/Home.elm
+++ src/elm/Home.elm
@@ -183,64 +183,6 @@ databaseName s =
         |> Maybe.withDefault ""
 
 
-type Connection
-    = DefaultConnection DefaultConnectionSetting
-    | SqliteConnection SqliteConnectionSetting
-
-
-connectionsDecoder : JD.Decoder (List Connection)
-connectionsDecoder =
-    JD.list connectionDecoder
-
-
-connectionDecoder : JD.Decoder Connection
-connectionDecoder =
-    JD.oneOf
-        [ JD.map DefaultConnection defaultConnectionSettingDecoder
-        , JD.map SqliteConnection sqliteConnectionSettingDecoder
-        ]
-
-
-type alias DefaultConnectionSetting =
-    { id : String
-    , system : String
-    , name : String
-    , hostname : String
-    , portStr : String
-    , username : String
-    , password : String
-    }
-
-
-defaultConnectionSettingDecoder : JD.Decoder DefaultConnectionSetting
-defaultConnectionSettingDecoder =
-    JD.map7 DefaultConnectionSetting
-        (JD.field "id" JD.string)
-        (JD.field "driver" JD.string)
-        (JD.field "name" JD.string)
-        (JD.field "hostname" JD.string)
-        (JD.field "port" JD.string)
-        (JD.field "username" JD.string)
-        (JD.field "password" JD.string)
-
-
-type alias SqliteConnectionSetting =
-    { id : String
-    , system : String
-    , name : String
-    , filepath : String
-    }
-
-
-sqliteConnectionSettingDecoder : JD.Decoder SqliteConnectionSetting
-sqliteConnectionSettingDecoder =
-    JD.map4 SqliteConnectionSetting
-        (JD.field "id" JD.string)
-        (JD.field "system" JD.string)
-        (JD.field "name" JD.string)
-        (JD.field "filepath" JD.string)
-
-
 type alias ErrorStatus =
     { errorModalOpen : Bool
     , lastErrorMessage : String
